f56853b825f431e50a8026c10f093a7079ab6ade
alexandersuglio <alexandersuglio@gmail.com>
2017-07-24 23:00:47 -0400

    working on client server routes

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ $('#showIt').on("click", function() {
 
         for (var k = 0; k < data.length; k++) {
 
-            $('#articlesHere').append("<div id=" + data[k]._id + "> <h1>" + data[k].title + "</h1>" + "<p> Written by: " + data[k].author + "<p> <p>Summary: " + data[k].summary + "</p></div><button class='delete'> Delete from List </button>");
+            $('#articlesHere').append("<div class='articleBody' id=" + data[k]._id + "> <h1>" + data[k].title + "</h1>" + "<p> Written by: " + data[k].author + "<p> <p>Summary: " + data[k].summary + "</p><button class='delete'> Delete from List </button></div>");
 
         };
 
@@ -18,3 +18,13 @@ $('#scrape').on('click', function(){
 $.getJSON('/scrape');
 
 });
+
+$('.delete').on('click', '.articleBody', function(){
+
+alert('you clicked delete');
+
+});
+
+
+
+
